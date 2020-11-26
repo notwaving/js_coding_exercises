8f6fe321d744a11f828e10dca811275487a7ccc1
notwaving <samanthaburke.uk@gmail.com>
2020-11-26 19:19:47 +0000

    capitalize function passes

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,53 +1,52 @@
 function capitalize(word) {
-  if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
 function generateInitials(firstName, lastName) {
-  if (firstName === undefined) throw new Error("firstName is required");
-  if (lastName === undefined) throw new Error("lastName is required");
+  if (firstName === undefined) throw new Error('firstName is required');
+  if (lastName === undefined) throw new Error('lastName is required');
   // Add your code here!
 }
 
 function addVAT(originalPrice, vatRate) {
-  if (originalPrice === undefined) throw new Error("originalPrice is requied");
-  if (vatRate === undefined) throw new Error("vatRate is required");
+  if (originalPrice === undefined) throw new Error('originalPrice is requied');
+  if (vatRate === undefined) throw new Error('vatRate is required');
   // Add your code here!
 }
 
 function getSalePrice(originalPrice, reduction) {
-  if (originalPrice === undefined) throw new Error("originalPrice is required");
-  if (reduction === undefined) throw new Error("reduction is required");
+  if (originalPrice === undefined) throw new Error('originalPrice is required');
+  if (reduction === undefined) throw new Error('reduction is required');
   // Add your code here!
 }
 
 function getMiddleCharacter(str) {
-  if (str === undefined) throw new Error("str is required");
+  if (str === undefined) throw new Error('str is required');
   // Add your code here!
 }
 
 function reverseWord(word) {
-  if (word === undefined) throw new Error("word is required");
+  if (word === undefined) throw new Error('word is required');
   // Add your code here!
 }
 
 function reverseAllWords(words) {
-  if (words === undefined) throw new Error("words is required");
+  if (words === undefined) throw new Error('words is required');
   // Add your code here!
 }
 
 function countLinuxUsers(users) {
-  if (users === undefined) throw new Error("users is required");
+  if (users === undefined) throw new Error('users is required');
   // Add your code here!
 }
 
 function getMeanScore(scores) {
-  if (scores === undefined) throw new Error("scores is required");
+  if (scores === undefined) throw new Error('scores is required');
   // Add your code here!
 }
 
 function simpleFizzBuzz(n) {
-  if (n === undefined) throw new Error("n is required");
+  if (n === undefined) throw new Error('n is required');
   // Add your code here!
 }
 
@@ -61,5 +60,5 @@ module.exports = {
   reverseAllWords,
   countLinuxUsers,
   getMeanScore,
-  simpleFizzBuzz
+  simpleFizzBuzz,
 };
