f96c7dc6f432be8625373e356279578b9e811899
notwaving <samanthaburke.uk@gmail.com>
2020-11-27 19:18:30 +0000

    getSquares and camelCaseWords pass

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,27 +1,45 @@
 function getSquares(nums) {
-  if (nums === undefined) throw new Error("nums is required");
-  // Your code here!
+  if (nums === undefined) throw new Error('nums is required');
+  let arrSquared = [];
+  let i;
+
+  for (i = 0; i < nums.length; i++) {
+    arrSquared.push(nums[i] * nums[i]);
+  }
+  return arrSquared;
 }
 
 function camelCaseWords(words) {
-  if (words === undefined) throw new Error("words is required");
-  // Your code here!
+  if (words === undefined) throw new Error('words is required');
+  let camelCased = [];
+  camelCased.push(words[0]);
+
+  let wordsToChange = words.slice(1);
+
+  let i;
+
+  for (i = 0; i < wordsToChange.length; i++) {
+    camelCased.push(
+      wordsToChange[i].charAt(0).toUpperCase() + wordsToChange[i].slice(1)
+    );
+  }
+  return camelCased.join('');
 }
 
 function getTotalSubjects(people) {
-  if (people === undefined) throw new Error("people is required");
+  if (people === undefined) throw new Error('people is required');
   // Your code here!
 }
 
 function checkIngredients(menu, ingredient) {
-  if (menu === undefined) throw new Error("menu is required");
-  if (!ingredient) throw new Error("ingredient is required");
+  if (menu === undefined) throw new Error('menu is required');
+  if (!ingredient) throw new Error('ingredient is required');
   // Your code here!
 }
 
 function duplicateNumbers(arr1, arr2) {
-  if (arr1 === undefined) throw new Error("arr1 is required");
-  if (arr2 === undefined) throw new Error("arr2 is required");
+  if (arr1 === undefined) throw new Error('arr1 is required');
+  if (arr2 === undefined) throw new Error('arr2 is required');
   // Your code here!
 }
 
@@ -30,5 +48,5 @@ module.exports = {
   camelCaseWords,
   getTotalSubjects,
   checkIngredients,
-  duplicateNumbers
+  duplicateNumbers,
 };
