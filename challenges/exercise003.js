abbb7ded4c88857d44c376c011b1a9e78e2b25ac
notwaving <samanthaburke.uk@gmail.com>
2020-11-27 20:12:47 +0000

    adds most recent progress

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -52,7 +52,12 @@ function checkIngredients(menu, ingredient) {
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error('arr1 is required');
   if (arr2 === undefined) throw new Error('arr2 is required');
-  // Your code here!
+
+  let z = arr1.filter(function (val) {
+    return arr2.indexOf(val) != -1;
+  });
+
+  return z;
 }
 
 module.exports = {
