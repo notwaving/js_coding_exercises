6e2b3d2d463d6b5892c3b379c887bc5f07c54e3e
notwaving <samanthaburke.uk@gmail.com>
2020-12-03 18:16:00 +0000

    smallNums passing with for loop

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,42 +1,51 @@
 function findSmallNums(nums) {
-  if (!nums) throw new Error("nums is required");
-  // Your code here
+  if (!nums) throw new Error('nums is required');
+  // Looping through the nums array, return all items smaller than 1
+  let smallNums = [];
+  let i;
+
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] < 1) {
+      smallNums.push(nums[i]);
+    }
+  }
+  return smallNums;
 }
 
 function findNamesBeginningWith(names, char) {
-  if (!names) throw new Error("names is required");
-  if (!char) throw new Error("char is required");
+  if (!names) throw new Error('names is required');
+  if (!char) throw new Error('char is required');
   // Your code here
 }
 
 function findVerbs(words) {
-  if (!words) throw new Error("words is required");
+  if (!words) throw new Error('words is required');
   // Your code here
 }
 
 function getIntegers(nums) {
-  if (!nums) throw new Error("nums is required");
+  if (!nums) throw new Error('nums is required');
   // Your code here
 }
 
 function getCities(users) {
-  if (!users) throw new Error("users is required");
+  if (!users) throw new Error('users is required');
   // Your code here
 }
 
 function getSquareRoots(nums) {
-  if (!nums) throw new Error("nums is required");
+  if (!nums) throw new Error('nums is required');
   // Your code here
 }
 
 function findSentencesContaining(sentences, str) {
-  if (!sentences) throw new Error("sentences is required");
-  if (!str) throw new Error("str is required");
+  if (!sentences) throw new Error('sentences is required');
+  if (!str) throw new Error('str is required');
   // Your code here
 }
 
 function getLongestSides(triangles) {
-  if (!triangles) throw new Error("triangles is required");
+  if (!triangles) throw new Error('triangles is required');
   // Your code here
 }
 
@@ -48,5 +57,5 @@ module.exports = {
   getCities,
   getSquareRoots,
   findSentencesContaining,
-  getLongestSides
+  getLongestSides,
 };
