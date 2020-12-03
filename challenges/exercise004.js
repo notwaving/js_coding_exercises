ca98c54877cfc45954203c6f89da376aeca6179a
notwaving <samanthaburke.uk@gmail.com>
2020-12-03 18:49:28 +0000

    findNamesBeginningWith passes with loop

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -15,7 +15,15 @@ function findSmallNums(nums) {
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error('names is required');
   if (!char) throw new Error('char is required');
-  // Your code here
+  let beginsWithChar = [];
+  let i;
+
+  for (i = 0; i < names.length; i++) {
+    if (names[i].charAt(0) === char) {
+      beginsWithChar.push(names[i]);
+    }
+  }
+  return beginsWithChar;
 }
 
 function findVerbs(words) {
