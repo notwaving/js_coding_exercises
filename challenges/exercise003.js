41713b4e41660fd3efed708ad826a02ec0361959
notwaving <samanthaburke.uk@gmail.com>
2020-12-05 00:49:36 +0000

    converts all functions to arrow functions

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,4 +1,4 @@
-function getSquares(nums) {
+getSquares = nums => {
   if (nums === undefined) throw new Error('nums is required');
   let arrSquared = [];
   let i;
@@ -7,9 +7,9 @@ function getSquares(nums) {
     arrSquared.push(nums[i] * nums[i]);
   }
   return arrSquared;
-}
+};
 
-function camelCaseWords(words) {
+camelCaseWords = words => {
   if (words === undefined) throw new Error('words is required');
 
   let camelCased = [];
@@ -17,7 +17,7 @@ function camelCaseWords(words) {
   camelCased.push(words[0]);
 
   // Create new array, with first word removed
-  let wordsToChange = words.slice(1);
+  const wordsToChange = words.slice(1);
   let i;
 
   for (i = 0; i < wordsToChange.length; i++) {
@@ -26,9 +26,9 @@ function camelCaseWords(words) {
     );
   }
   return camelCased.join('');
-}
+};
 
-function getTotalSubjects(people) {
+getTotalSubjects = people => {
   if (people === undefined) throw new Error('people is required');
   // This seems like a very roundabout way of finding the answer.
   // With more time I'd like to experiment with the more powerful functions
@@ -39,11 +39,11 @@ function getTotalSubjects(people) {
     count.push(people[i].subjects);
   }
   // At the moment we have a multidimensional array, and we need to flatten it so everything gets counted.
-  var flattened = [].concat.apply([], count);
+  const flattened = [].concat.apply([], count);
   return flattened.length;
-}
+};
 
-function checkIngredients(menu, ingredient) {
+checkIngredients = (menu, ingredient) => {
   if (menu === undefined) throw new Error('menu is required');
   if (!ingredient) throw new Error('ingredient is required');
 
@@ -53,9 +53,9 @@ function checkIngredients(menu, ingredient) {
   // console.log (storeCupboard.flat());
   let answer = storeCupboard.includes(ingredient);
   return answer;
-}
+};
 
-function duplicateNumbers(arr1, arr2) {
+duplicateNumbers = (arr1, arr2) => {
   if (arr1 === undefined) throw new Error('arr1 is required');
   if (arr2 === undefined) throw new Error('arr2 is required');
   // Something with filter method?
@@ -64,7 +64,7 @@ function duplicateNumbers(arr1, arr2) {
   // });
 
   // return z;
-}
+};
 
 module.exports = {
   getSquares,
