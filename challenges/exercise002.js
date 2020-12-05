a907cfd9490faa1c1fef88799194dc55bdaa11a5
notwaving <samanthaburke.uk@gmail.com>
2020-12-05 00:42:13 +0000

    converts all functions to arrow functions, introduces ternaries

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,41 +1,33 @@
-function getFillings(sandwich) {
+getFillings = sandwich => {
   if (sandwich === undefined) throw new Error('ingredients is required');
   return sandwich.fillings;
-}
+};
 
-function isFromManchester(person) {
+isFromManchester = person => {
   if (person === undefined) throw new Error('person is required');
-  if (person.city === 'Manchester') {
-    return true;
-  } else {
-    return false;
-  }
-}
 
-function getBusNumbers(people) {
+  return person.city === 'Manchester' ? true : false;
+};
+
+getBusNumbers = people => {
   if (people === undefined) throw new Error('people is required');
   return Math.ceil(people / 40);
-}
+};
 
-function countSheep(arr) {
+countSheep = arr => {
   if (arr === undefined) throw new Error('arr is required');
-  // Let's filter this array!
-  return arr.filter(x => x == 'sheep').length;
-}
 
-function hasMPostCode(person) {
+  return arr.filter(x => x == 'sheep').length;
+};
+
+hasMPostCode = person => {
   if (person === undefined) throw new Error('person is required');
 
-  // Check postCode for letter "M"
-  if (
-    person.address.postCode.charAt(0) === 'M' &&
+  return person.address.postCode.charAt(0) === 'M' &&
     person.address.city === 'Manchester'
-  ) {
-    return true;
-  } else {
-    return false;
-  }
-}
+    ? true
+    : false;
+};
 
 module.exports = {
   getFillings,
