64505809429afe8ed38d89d77473f5fc7374f3e9
notwaving <samanthaburke.uk@gmail.com>
2020-12-22 15:15:46 +0000

    adds first test for getScreentimeAlertList

--- test/exercise007.test.js
+++ test/exercise007.test.js
@@ -1,7 +1,7 @@
 const {
   sumDigits,
   createRange,
-  //getScreentimeAlertList,
+  getScreentimeAlertList,
   //hexToRGB,
   //findWinner,
 } = require('../challenges/exercise007');
@@ -31,7 +31,54 @@ describe('createRange', () => {
   });
 });
 
-// describe('getScreentimeAlertList', () => {});
+describe('getScreentimeAlertList', () => {
+  const users = [
+    {
+      username: 'beth_1234',
+      name: 'Beth Smith',
+      screenTime: [
+        {
+          date: '2019-05-01',
+          usage: { twitter: 34, instagram: 22, facebook: 40 },
+        },
+        {
+          date: '2019-05-02',
+          usage: { twitter: 56, instagram: 40, facebook: 31 },
+        },
+        {
+          date: '2019-05-03',
+          usage: { twitter: 12, instagram: 15, facebook: 19 },
+        },
+        {
+          date: '2019-05-04',
+          usage: { twitter: 10, instagram: 56, facebook: 61 },
+        },
+      ],
+    },
+    {
+      username: 'sam_j_1989',
+      name: 'Sam Jones',
+      screenTime: [
+        {
+          date: '2019-06-11',
+          usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 10 },
+        },
+        {
+          date: '2019-06-13',
+          usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 16 },
+        },
+        {
+          date: '2019-06-14',
+          usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 31 },
+        },
+      ],
+    },
+  ];
+
+  test('Both the start and the end numbers are inclusive', () => {
+    expect(getScreentimeAlertList(users, '2019-05-02')).toEqual(['beth_1234']);
+  });
+});
 
 // describe('hexToRGB', () => {});
 
